d2320f11044e0f50e2d33829876422bf4af2448d
Josh265365 <salijenijoshuq@gmail.com>
2022-04-02 15:40:40 +0100

    Add project files.

---
+++ Lab9-10/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab9_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<float> inputData = new List<float>();
+
+            bool moreData = true;
+            while (moreData)
+            {
+                // Prompt the user to enter the data 
+                Console.Write("Enter a value for the distribution.");
+                float number;
+
+                while (!float.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.Write("Please enter a number.");
+                }
+
+                // add the data to the list in the distribution 
+                inputData.Add(number);
+
+                // Prompt user to enter another data 
+                string inputString;
+                do
+                {
+                    Console.Write("Key in another data? Y/N.");
+                    inputString = Console.ReadLine().ToUpper();
+                } while (!(inputString == "Y" || inputString == "N"));
+
+                // Stop data entry loop once the user Enters No. 
+                if (inputString == "N")
+                {
+                    moreData = false;
+                }
+            }
+
+            // convert the List to array to be processed by the Skewness object 
+            float[] myData = inputData.ToArray();
+
+            // Instantiate an object of the skewness class. 
+            Skewness mySkewness = new Skewness(myData);
+
+            // Print the results of methods inherited from the Statistics class 
+            Console.WriteLine("Mean of Distribution: " + mySkewness.Mean.ToString("F2"));
+
+            Console.WriteLine("Median of Distribution: " + mySkewness.Median.ToString("F2"));
+
+
+            Console.WriteLine("Mode of Distribution: " + mySkewness.Mode.ToString("F2"));
+
+            Console.WriteLine("Standard Deviation of Distribution: " + mySkewness.StandardDeviation.ToString("F2"));
+
+            // Print the results of methods in Skewness class 
+            Console.WriteLine("Pearson Mode Skewness: " + mySkewness.PearsonModeSkewness.ToString("F2"));
+
+
+            Console.WriteLine("Pearson Median Skewness: " + mySkewness.PearsonMedianSkewness.ToString("F2"));
+
+
+        }
+    }
+}
---
+++ Lab9-10/Skewness.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasyStatistics; // using created ddl file
+
+namespace Lab9_10
+{
+    class Skewness : Statistics // inheritng from ddl
+    {
+        private float _pearsonModeSkewness;
+        private float _pearsonMedianSkewness; // member variables
+
+        public float PearsonModeSkewness
+        {
+            get { return _pearsonModeSkewness; }
+            private set { _pearsonModeSkewness = value; }
+        }
+
+        public float PearsonMedianSkewness
+        {
+            get { return _pearsonMedianSkewness; }
+            private set { _pearsonMedianSkewness = value; }
+        }// class properties
+
+        public Skewness(float[] pData) : base(pData)
+        {
+            if (pData == null)
+            {
+                throw new ArgumentNullException(nameof(pData));
+            }
+            else
+            {
+                // Compute Pearson Mode Skewness 
+                ComputePearsonModeSkewness(pData);
+
+                // Compute Pearson Median Skewness 
+                ComputePearsonMedianSkewness(pData);
+            }
+        }
+
+        private void ComputePearsonMedianSkewness(float[] pData)
+        {
+            PearsonModeSkewness = (Mean - Mode) / StandardDeviation;
+        }
+
+        private void ComputePearsonModeSkewness(float[] pData)
+        {
+            PearsonMedianSkewness = (3 * (Mean - Median)) / StandardDeviation;
+        }// clalcualting pearson statistics using variables from the ddl file 
+
+
+    }
+}
